c1d9af3a3fdd8471cbc576813925404646d78550
DanhTrannn <tranthanhdanhktvn2005d@gmail.com>
2024-10-19 23:04:06 +0700

    add savedata and loaddata

--- Database/SaveData.cs
+++ Database/SaveData.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DataStructure;
 namespace Database
 {
-    internal class Program
+    internal class SaveData
     {
         static void Main(string[] args)
         {
