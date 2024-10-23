38d951e6a8c50f093766d9ea806930b68ee1482a
DanhTrannn <tranthanhdanhktvn2005d@gmail.com>
2024-10-23 20:21:05 +0700

    .

--- Showtime/Program.cs
+++ Showtime/Program.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Database;
 using DataStructure;
-namespace Movies
+namespace Showtime
 {
-    public class Timkiemphim
+    public class Program
     {
     }
 }
