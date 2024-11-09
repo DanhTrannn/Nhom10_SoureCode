1109f50e51b1751fd0ae241031c7890c7ca7802c
pb110105 <161494686+pb110105@users.noreply.github.com>
2024-11-09 13:33:12 +0700

    Hoàn thánh chức năng cơ bản MoviesManager.cs

--- Movies/MoviesManager.cs
+++ Movies/MoviesManager.cs
@@ -1,18 +1,69 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Database;
 using DataStructure;
-namespace Movies
+using System.IO;
+using System.Diagnostics.Eventing.Reader;
+
+public class MovieManager
 {
-    public class MoviesManager
+    private DataStructure _data;
+
+    public MovieManager(DataStructure data)
     {
-        private datastructure _data;
-        public MoviesManager(datastructure data)
+        _data = data;
+    }
+
+    public void AddMovie(Movie movie) => _data.Movies.Add(movie);
+    public void RemoveMovie(string id) => _data.Movies.RemoveAll(m => m.Id == id);
+    public void UpdateMovie(Movie updatedMovie)
+    {
+        for (int i = 0; i < _data.Movies.Count; i++)
         {
-            this._data = data;
+            if (_data.Movies[i].Id == updatedMovie.Id)
+            {
+                _data.Movies[i] = updatedMovie;
+                break;
+            }
         }
     }
+
+    public void SaveMovies()
+    {
+        using (var writer = new StreamWriter(Database.movieFilePath))
+        {
+            foreach (var movie in _data.Movies)
+            {
+                writer.WriteLine($"{movie.Id},{movie.Title},{movie.Genre},{movie.Duration}");
+            }
+        }
+    }
+
+    public void LoadMovies()
+    {
+        if (File.Exists(Database.movieFilePath))
+        {
+            using (var reader = new Streamer(Databse.movieFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var parts = line.Split(',');
+                    if (parts.Length == 4)
+                    {
+                        var movie = new Movie(parts[0], parts[1], parts[2], parts[3]);
+                        _data.Movies.Add(movie);
+                    }
+                }
+            }
+        }
+    }
+}
+
+public static class Database
+{
+    public static string movieFilePath = "movíes.txt";
 }
